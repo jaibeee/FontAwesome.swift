605dc40b2f80b962be1a51c3e561326064e81436
Jamalah Bryan <jaibeee@gmail.com>
2016-07-30 14:58:49 +0100

    Added UIButton IBDesignable support.

--- FontAwesome/FontAwesomeButton.swift
+++ FontAwesome/FontAwesomeButton.swift
@@ -1,30 +1,15 @@
-// FontAwesomeView.swift
 //
-// Copyright (c) 2016 Antony Alkmim
+//  FontAwesomeButton.swift
+//  FontAwesome
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//  Created by Jai Bryan on 30/07/2016.
 //
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
 
 import UIKit
 
-/// A view for FontAwesome icons.
-@IBDesignable class FontAwesomeView : UIView {
-    
+class FontAwesomeButton: UIButton {
+
     @IBInspectable
     var iconCode:String = "" {
         didSet{
@@ -69,5 +54,5 @@ import UIKit
         self.clipsToBounds = true
         iconView.frame = CGRectMake(0, 0, bounds.size.width, bounds.size.height)
     }
-    
+
 }
